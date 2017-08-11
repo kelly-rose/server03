a5b229cef6dc3b1d4cbab90893aec6d784fa38de
kelly <kellyrose.evergreen@gmail.com>
2017-08-11 12:54:05 +0900

    4th commit : ~ section 8-104

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const cookieSession = require('cookie-session');
 const passport = require('passport');
@@ -14,6 +15,7 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(
     cookieSession({
         maxAge:30 * 24 * 60 *1000,
@@ -25,6 +27,7 @@ app.use(passport.initialize()); //tell passport that we use cookie
 app.use(passport.session());
 
 authRoutes(app);
+require('./routes/billingRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
---
+++ routes/billingRoutes.js
@@ -0,0 +1,25 @@
+const keys = require('../config/keys');
+const stripe=require('stripe')(keys.stripeSecretKey);
+
+module.exports=app=>{
+    app.post('/api/stripe',async (req,res)=>{
+        // console.log(req.body);
+
+        if(!req.user){
+            return res.status(401).send({error:'You must log in!'});
+        }
+      const charge= await stripe.charges.create({
+            amount:500,
+            currency:'usd',
+            description:'$5 for 5 credits',
+            source:req.body.id
+        });
+
+      // console.log(charge);
+        req.user.credits += 5;  //section 8-103
+        const user = await req.user.save();
+
+        res.send(user);
+
+    });
+};
